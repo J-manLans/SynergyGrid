b6ad3256f69d1d67da17e60c61c1efc50a1f2603
Lauri <labe2301@student.miun.se>
2026-02-12 23:04:01 +0100

    Implement command arguments
    One will be able to pass training arguments via the command line
    
    --env <environment id> (defaults to 0)
    
    --alg <algorithm id> (defaults to 0)
    
    --train (train the agent). If left out, will run instead of train
    
    --timesteps <number of timesteps> (default 10_000)
    
    --iterations <number of iterations> (default 10_000)

--- src/experiments/__main__.py
+++ src/experiments/__main__.py
@@ -1,4 +1,46 @@
+import argparse
 from experiments import GymAgentRunner, environments, algorithms
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run agent experiments.")
+
+    parser.add_argument(
+        "--env",
+        type=int,
+        default=0,
+        choices=range(len(environments)),
+        help="Environment index"
+    )
+
+    parser.add_argument(
+        "--alg",
+        type=int,
+        default=0,
+        choices=range(len(algorithms)),
+        help="Algorithm index"
+    )
+
+    parser.add_argument(
+        "--train",
+        action="store_true",
+        help="Train the agent"
+    )
+
+    parser.add_argument(
+        "--timesteps",
+        type=int,
+        default=10000,
+        help="Number of timesteps"
+    )
+
+    parser.add_argument(
+        "--iterations",
+        type=int,
+        default=10000,
+        help="Number of iterations"
+    )
+
+    return parser.parse_args()
 
 def main():
     # Chose environment
@@ -10,18 +52,25 @@ def main():
     # 5: MountainCar-v0
     # 6: CliffWalking-v1
     # 7: FrozenLake-v1
-    env = list(environments.keys())
-    environment = env[1]
+
+    #env = list(environments.keys())
+    #environment = env[0]
+    
     # Choose algorithm to use for training (also needed when running agent)
     # 0: PPO
     # 1: DQN
-    alg = list(algorithms.keys())
-    algorithm = alg[0]
-    agent = True  # Choose to use an agent or not
-    training = False  # Choose to train or run the agent
-    timesteps = 220000  # Num of timesteps for training or model selection when running
-    iterations = 6  # Number of training iterations
 
+    #algorithm = alg[0]
+
+    args = parse_args()
+    # pyhon -m experiments --env 0 --alg 0 --train --timesteps 10000 --iterations 10000
+
+    environment = list(environments.keys())[args.env] # Choose the environment
+    algorithm = list(algorithms.keys())[args.alg] # Choose the algorithm
+    training = args.train # Choose to train or run the agent
+    timesteps = args.timesteps # Choose the number of timesteps
+    iterations = args.iterations # Choose the number of iterations
+    agent = True  # Choose to use an agent or not
     runner = GymAgentRunner(environment=environment, algorithm=algorithm)
 
     if agent:
