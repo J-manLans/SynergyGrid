d434fbf9d6d7dffb9e678a1d045cca7977cd9b18
Joel Lansgren <lansgren84@gmail.com>
2026-02-19 18:04:50 +0200

    Completed simple pygame setup

--- src/synergygrid/core/grid_world.py
+++ src/synergygrid/core/grid_world.py
@@ -1,5 +1,8 @@
 from enum import Enum
 from numpy.random import Generator, default_rng
+import pygame
+import sys
+from os import path
 
 
 # Actions the Agent is capable of performing i.e. go in a certain direction
@@ -22,11 +25,59 @@ class GridTile(Enum):
 
 
 class GridWorld:
-    def __init__(self, grid_rows=5, grid_cols=5, starting_score=10):
+    def __init__(self, grid_rows=5, grid_cols=5, starting_score=10, fps=1):
         self.grid_rows = grid_rows
         self.grid_cols = grid_cols
         self.starting_score = starting_score
         self.reset()
+
+        self.fps = fps
+        self.last_action = ""
+        self.__init_pygame()
+
+    def __init_pygame(self):
+        self.__init_colors()  # Initialize colors
+        pygame.init()  # Initialize pygame
+        pygame.display.init()  # initialize the display module
+
+        self.clock = pygame.time.Clock()  # Game clock
+
+        # Default font
+        self.action_font = pygame.font.SysFont("Calibre", 30)
+        self.action_info_height = self.action_font.get_height()
+
+        # For rendering
+        self.cell_width = 64
+        self.cell_height = 64
+        self.cell_size = (self.cell_width, self.cell_height)
+
+        # Define game window size (width, height)
+        self.window_size = (
+            self.cell_width * self.grid_cols,
+            self.cell_height * self.grid_rows + self.action_info_height,
+        )
+
+        # Initialize game window
+        self.window_surface = pygame.display.set_mode(self.window_size)
+
+        # Load & resize sprites
+        ROOT_DIR = path.abspath(path.join(path.dirname(__file__), "..", "..", ".."))
+
+        file_name = path.join(ROOT_DIR, "assets/sprites/agent.png")
+        img = pygame.image.load(file_name)
+        self.agent_img = img
+
+        file_name = path.join(ROOT_DIR, "assets/sprites/green-resource.png")
+        img = pygame.image.load(file_name)
+        self.green_resource_img = img
+
+        file_name = path.join(ROOT_DIR, "assets/tiles/floor.png")
+        img = pygame.image.load(file_name)
+        self.floor_img = img
+
+    def __init_colors(self):
+        self.background_clr = (45, 29, 29)
+        self.text_clr = (255, 246, 213)
 
     def reset(self, rng: Generator | None = None):
         if rng is None:
@@ -40,8 +91,7 @@ class GridWorld:
         ]
 
     def perform_action(self, agent_action: AgentAction) -> bool:
-        # Will see at what part this becomes necessary
-        # self.last_action = agent_action
+        self.last_action = agent_action
 
         # Move Robot to the next cell
         if agent_action == AgentAction.LEFT:
@@ -61,15 +111,57 @@ class GridWorld:
         return self.agent_pos == self.resource_pos
 
     def render(self):
-        # Print current state to console
+        self._process_events()
+
+        self.window_surface.fill(self.background_clr)
+
+        # Draw the graphics with pygame
         for r in range(self.grid_rows):
             for c in range(self.grid_cols):
-                if [r, c] == self.agent_pos:
-                    print(GridTile.AGENT, end=" ")
-                elif [r, c] == self.resource_pos:
-                    print(GridTile.RESOURCE, end=" ")
-                else:
-                    print(GridTile._FLOOR, end=" ")
+                # Draw floor
+                pos = (c * self.cell_width, r * self.cell_height)
+                self.window_surface.blit(self.floor_img, pos)
 
-            print()  # New line
-        print()  # New line
+                if [r, c] == self.resource_pos:
+                    self.window_surface.blit(self.green_resource_img, pos)
+
+                if [r, c] == self.agent_pos:
+                    self.window_surface.blit(self.agent_img, pos)
+
+        text = self.action_font.render(
+            f"Action: {self.last_action}", True, self.text_clr, self.background_clr
+        )
+        text_pos = (0, self.window_size[1] - self.action_info_height)
+        self.window_surface.blit(text, text_pos)
+
+        pygame.display.update()
+        self.clock.tick(self.fps)  # Limits fps
+
+    def _process_events(self):
+        # Process user events, key presses
+        for event in pygame.event.get():
+            # User clicked on X at the top right corner of window
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                # User hit escape
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+
+# For testing the graphics, remove when working
+if __name__ == "__main__":
+    grid_world = GridWorld()
+    grid_world.render()
+    rng = default_rng()
+
+    while True:
+        actions = list(AgentAction)
+        rand_index = rng.integers(0, len(actions))
+        rand_action = actions[rand_index]
+
+        grid_world.perform_action(rand_action)
+        grid_world.render()
