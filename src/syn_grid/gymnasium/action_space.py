efcc753c51deecea188b131c3f53591b7d4c68d1
Joel Lansgren <lansgren84@gmail.com>
2026-04-10 00:33:47 +0300

    Added a little file structure.

--- src/syn_grid/gymnasium/action_space.py
+++ src/syn_grid/gymnasium/action_space.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class DroidAction(Enum):
-    """Actions the droid is capable of performing i.e. go in a certain direction"""
+    """Actions the droid is capable of performing i.e. go in the four cardinal directions"""
 
     LEFT = 0
     DOWN = 1
